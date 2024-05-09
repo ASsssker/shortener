022ac17750bfd7dbe82bd36bfa548feb8e5d0583
Asker <ask.mol2@mail.ru>
2024-05-09 23:58:51 +0300

    Добавлен middleware для логирования информации о запросе

--- cmd/server/middlewares.go
+++ cmd/server/middlewares.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-// RequestsInfo логгирует информацию о запросе
+// RequestsInfo логирует информацию о запросе
 func (a *Application) RequestsInfo(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		lrw := &loggingResponseWriter{
